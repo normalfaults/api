7e726ff96f21172507b7b971d384b9ffe506f9ac
Chris Kacerguis <kacerguis_christopher@bah.com>
2014-11-03 19:48:44 -0600

    output raw ruby code

--- app/controllers/automate_controller.rb
+++ app/controllers/automate_controller.rb
@@ -1,8 +1,17 @@
 # Display automate code for ManageIQ / CloudForms
 class AutomateController < ApplicationController
+  layout false
   def catalog_item_initialization
+    file      = File.open("app/views/automate/catalog_item_initialization.html.erb", "r")
+    contents  = file.read
+    file.close
+    render plain: contents
   end
 
   def update_servicemix_and_chef
+    file      = File.open("app/views/automate/update_servicemix_and_chef.html.erb", "r")
+    contents  = file.read
+    file.close
+    render plain: contents
   end
 end
