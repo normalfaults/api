ada5ce76e8f5cfe1c7608b1d61a10a9cc2e21b81
Chris Kacerguis <kacerguis_christopher@bah.com>
2014-11-04 10:24:00 -0600

    apease rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,12 +3,14 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 # Test Case
-#class FooBarTest < ActiveSupport::TestCase
-#class ActiveSupport
-class ActiveSupport::TestCase
-  def test_case
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    # fixtures :all
+# class FooBarTest < ActiveSupport::TestCase
+# class ActiveSupport
+class ActiveSupport
+  class TestCase
+    def test_case
+      # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+      # fixtures :all
+    end
   end
   # Add more helper methods to be used by all tests here...
 end
