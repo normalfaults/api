774199a6de260795dd0ac2f6bb2706e1397c4a7d
Chris Kacerguis <kacerguis_christopher@bah.com>
2014-11-11 18:32:41 -0600

    added welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   # Notifications Routes
   get 'notifications', to: 'notifications#index'
 
@@ -14,7 +16,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
