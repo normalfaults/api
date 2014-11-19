f8953ab03c1aa5d1aada1e5de92c8c176406e1c1
Christopher Parratto <cparratto@gmail.com>
2014-11-19 13:32:13 -0600

    Better json errors for login. Addressed rubocop issues.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,6 @@
 class SessionsController < Devise::SessionsController
   extend Apipie::DSL::Concern
 
-  respond_to :json
-
   api :POST, '/staff/sign_in', 'Signs user in'
   param :staff, Hash, desc: 'Staff' do
     param :email, String, desc: 'Email'
@@ -10,19 +8,38 @@ class SessionsController < Devise::SessionsController
   end
 
   def create
-    self.resource = warden.authenticate!(auth_options)
-    sign_in(resource_name, resource)
-    respond_with resource
+    respond_to do |format|
+      format.html do
+        super
+      end
+      format.json do
+        self.resource = warden.authenticate(auth_options)
+
+        if resource
+          sign_in(resource_name, resource)
+          render json: resource
+        else
+          render json: { error: 'Invalid Login' }, status: 401
+        end
+      end
+    end
   end
 
   api :DELETE, '/staff/sign_out', 'Invalidates user session'
 
   def destroy
-    if resource
-      Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-      render json: {}, status: :ok
-    else
-      render json: {}, status: :unprocessable_entity
+    respond_to do |format|
+      format.html do
+        super
+      end
+      format.json do
+        if resource
+          Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+          render json: {}, status: :ok
+        else
+          render json: {}, status: :unprocessable_entity
+        end
+      end
     end
   end
 end
