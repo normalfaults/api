941c51d3553bb97050b712f90c904c46dda1da71
Christopher Parratto <cparratto@gmail.com>
2014-11-30 17:09:52 -0600

    Added default scope for ordering project questions.

--- spec/factories/project_question.rb
+++ spec/factories/project_question.rb
@@ -1,13 +1,20 @@
 FactoryGirl.define do
   factory :project_question do
     question 'Question?'
-    field_type :text
+    field_type :select_option
     help_text 'helping'
     options %w(one tow three)
     required true
+    load_order 0
 
     trait :optional do
       required false
+    end
+
+    trait :required_text do
+      field_type :text
+      load_order 1
+      options %w()
     end
   end
 end
