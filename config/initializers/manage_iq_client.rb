aa6eacba5ca1042ca7c4e931408cd2d62ea37d7f
Michael Stack <michael.stack@gmail.com>
2014-12-04 01:42:09 -0600

    Rubocop violation

--- config/initializers/manage_iq_client.rb
+++ config/initializers/manage_iq_client.rb
@@ -1,6 +1,6 @@
 if ENV['MANAGEIQ_HOST']
   ManageIQClient.host = ENV['MANAGEIQ_HOST']
-  ManageIQClient.verify_ssl = ENV['MANAGEIQ_SSL'].nil? ? true : !!ENV['MANAGEIQ_SSL']
+  ManageIQClient.verify_ssl = ENV['MANAGEIQ_SSL'].nil? ? true : !ENV['MANAGEIQ_SSL'].to_i.zero?
   if ENV['MANAGEIQ_USER'] && ENV['MANAGEIQ_PASS']
     ManageIQClient.credentials = { user: ENV['MANAGEIQ_USER'], password: ENV['MANAGEIQ_PASS'] }
   end
