e37a524ec734412ba74e8db01dfaf6e4552bf69f
Christopher Parratto <cparratto@gmail.com>
2015-01-14 07:52:05 -0600

    Added support for token based session.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111172453) do
+ActiveRecord::Schema.define(version: 20150114130913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -284,8 +284,10 @@ ActiveRecord::Schema.define(version: 20150111172453) do
     t.inet     "last_sign_in_ip"
     t.integer  "role",                              default: 0
     t.datetime "deleted_at"
+    t.string   "authentication_token"
   end
 
+  add_index "staff", ["authentication_token"], name: "index_staff_on_authentication_token", using: :btree
   add_index "staff", ["deleted_at"], name: "index_staff_on_deleted_at", using: :btree
   add_index "staff", ["email"], name: "index_staff_on_email", unique: true, using: :btree
   add_index "staff", ["reset_password_token"], name: "index_staff_on_reset_password_token", unique: true, using: :btree
