1279a3a59f95155dc78e9a121fb49f6132f31e69
mafernando <fernando_michael@bah.com>
2015-01-19 11:52:16 -0600

    Change provision status from string to integer because it is an enum.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114130913) do
+ActiveRecord::Schema.define(version: 20150119174901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,7 +117,7 @@ ActiveRecord::Schema.define(version: 20150114130913) do
     t.integer  "cloud_id"
     t.integer  "product_id"
     t.integer  "service_id"
-    t.string   "provision_status"
+    t.integer  "provision_status"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
