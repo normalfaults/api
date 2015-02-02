0fc08f911b5622cd44d0e88ad84c94cd4b23e0ec
Jillian Tullo <tullo_jillian@bah.com>
2015-02-02 09:37:02 -0500

    Updated VM to find a specific configuration file rather than generating it through the code. Includes debugging class.

--- app/views/automate/create_vmware_vm.rb
+++ app/views/automate/create_vmware_vm.rb
@@ -11,7 +11,7 @@ class DebugClass
 end
 
 testHash = {
-  'dialog_instance_name' => 'Cloned_VM',
+  'dialog_instance_name' => 'Automate_Test_2',
   'dialog_num_CPUs' => 1,
   'dialog_memory_mb' => 4,
   'dialog_datastore_path' => '[datastore2]',
@@ -39,45 +39,13 @@ begin
   vim = RbVmomi::VIM.connect host: '173.203.165.9', port: 443, user: 'vsphere.local\administrator', password: 'P@ss1234', debug: true, insecure: true
   dc = vim.serviceInstance.find_datacenter(datacenter) or fail "Datacenter not found."
   $evm.log("info", "CreateVMWareVM: Found datacenter #{datacenter}")
-  configuration = {
-    name: instance_name,
-    numCPUs: num_CPUs,
-    memoryMB: memory_mb,
-    files: { vmPathName: datastore_path },
-    deviceChange: [
-      {
-        operation: :add,
-        device: RbVmomi::VIM.VirtualE1000(
-          key: 0,
-          deviceInfo: {
-            label: 'Network Adapter 1',
-              summary: 'VM Network'
-          },
-          backing: RbVmomi::VIM.VirtualEthernetCardNetworkBackingInfo(
-          deviceName: 'VM Network'
-          ),
-          addressType: 'generated'
-        )
-      }
-    ]
-  }
-  cust_ip_settings = RbVmomi::VIM::CustomizationIPSettings.new
-  cust_ip_settings.ip = RbVmomi::VIM::CustomizationFixedIp("ipAddress" => "10.0.0.1")
-  cust_adapter_mapping = [RbVmomi::VIM::CustomizationAdapterMapping.new("adapter" => cust_ip_settings)]
-  cust_prep = RbVmomi::VIM::CustomizationLinuxPrep.new(
-    domain: 'fake-test-this-can-fail',
-    hostName: 'fake-test-this-can-fail',
-    hwClockUTC: true,
-    timeZone: 'US/Eastern')
-  cust_global_ip_settings = RbVmomi::VIM::CustomizationGlobalIPSettings.new
-  custom_spec = RbVmomi::VIM::CustomizationSpec.new(
-    identity: cust_prep,
-    nicSettingMap: cust_adapter_mapping,
-    globalIPSettings: cust_global_ip_settings
-  )
-  cloneVM = dc.find_vm('VM_To_Clone')
-  pool = cloneVM.resourcePool
+  cloneVM = dc.find_vm('Template-2')
+  dc = vim.serviceInstance.find_datacenter('DC1')
+  configuration = RbVmomi::VIM.VirtualMachineConfigSpec(:annotation => 'Creation time:  ' + Time.now.strftime("%Y-%m-%d %H:%M") + "\n\n")
+  hosts = dc.hostFolder.children
+  pool = hosts.first.resourcePool
   relocation_spec = RbVmomi::VIM.VirtualMachineRelocateSpec(pool: pool)
+  custom_spec = vim.serviceContent.customizationSpecManager.GetCustomizationSpec(:name => 'rhel-dhcp').spec
   clone_spec =  RbVmomi::VIM.VirtualMachineCloneSpec(
     location: relocation_spec,
     config: configuration,
@@ -85,8 +53,7 @@ begin
     powerOn: true,
     template: false
   )
-  task = cloneVM.CloneVM_Task(:folder => cloneVM.parent, :name => 'Please-work', :spec => clone_spec)
-  $evm.log("error", "CloneVM: GOT EM!!!!!!!!")
+  task = cloneVM.CloneVM_Task(:folder => cloneVM.parent, :name => 'Automation-Test-2', :spec => clone_spec)
   task.wait_for_completion
 rescue StandardError => e
   $evm.log("error", "CloneVM: General exception caught: #{e.message}")
