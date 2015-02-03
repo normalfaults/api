ce6f005eabdc1ede11185138241f0cf228c8aa82
mafernando <fernando_michael@bah.com>
2015-02-02 19:27:49 -0600

    Fixes code climate observed sql vulnerability in Alerts model.

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -3,5 +3,5 @@ class Alert < ActiveRecord::Base
   belongs_to :project
   scope :active, -> { where('(alerts.start_date <= NOW() OR alerts.start_date IS NULL) AND (alerts.end_date >= NOW() OR alerts.end_date IS NULL)') }
   scope :recent, -> { joins('INNER JOIN (SELECT MAX(a3.id) AS id FROM alerts a3 GROUP BY order_item_id) a2 ON alerts.id = a2.id') }
-  scope :not_status, ->(status) { where("alerts.status != '#{status}'") }
+  scope :not_status, ->(status) { where('alerts.status != ?', status) }
 end
