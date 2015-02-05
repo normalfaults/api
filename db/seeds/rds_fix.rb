4e42c147f7e84d85fa08b65b32603a464a380289
Jerimiah Milton <jerimiah.milton@gmail.com>
2015-02-04 23:26:13 -0600

    Fixing rubocop errors.

--- db/seeds/rds_fix.rb
+++ db/seeds/rds_fix.rb
@@ -3,8 +3,8 @@ rds_type = ProductType.where(name: 'RDS').first
 
 rds_type_question = ProductTypeQuestion.where(product_type_id: rds_type.id, label: 'Storage Type').first
 rds_type_question.options = [
-    %w(standard Magnetic),
-    %w(gp2 SSD),
-    %w(io1 IOPS)
+  %w(standard Magnetic),
+  %w(gp2 SSD),
+  %w(io1 IOPS)
 ]
 rds_type_question.save
