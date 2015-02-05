c0bb5a877474b399ff7096a761922ee9c1feaf84
mafernando <fernando_michael@bah.com>
2015-02-04 23:41:58 -0600

    modified update_order_item in alert to set status of parent project, had to up cyclomatic complexity to appease rubocop

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -8,5 +8,22 @@ class Alert < ActiveRecord::Base
 
   def update_order_item
     order_item.update_attribute(:latest_alert_id, id)
+    project_status = :ok
+    order_item.project.latest_alerts.each do |latest_alert|
+      new_status = latest_alert.status
+      case new_status
+      when 'OK'
+        project_status = new_status unless [:pending, :unknown, :warning, :critical].include? project_status
+      when 'PENDING'
+        project_status = new_status unless [:warning, :critical].include? project_status
+      when %w(UNKNOWN WARNING)
+        project_status = :warning unless project_status == :critical
+      when 'CRITICAL'
+        project_status = new_status
+      else
+        project_status = :unknown
+      end
+    end
+    order_item.project.update_attribute(:status, project_status.downcase)
   end
 end
