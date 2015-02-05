59d35def55925fc2bdc165a9b03081e3d26b348a
Jerimiah Milton <jerimiah.milton@gmail.com>
2015-02-04 23:22:25 -0600

    Chaned id in payload to order_id. Added a seed file to correct the storage type values in the database.

---
+++ db/seeds/rds_fix.rb
@@ -0,0 +1,10 @@
+# Fix the RDS storage type options
+rds_type = ProductType.where(name: 'RDS').first
+
+rds_type_question = ProductTypeQuestion.where(product_type_id: rds_type.id, label: 'Storage Type').first
+rds_type_question.options = [
+    %w(standard Magnetic),
+    %w(gp2 SSD),
+    %w(io1 IOPS)
+]
+rds_type_question.save
