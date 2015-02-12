fe1f4838d4071b6f11f03581d1bc6161d987ea9a
Jillian Tullo <tullo_jillian@bah.com>
2015-02-12 08:14:06 -0500

    Changes to commit

--- app/views/automate/create_chef_node.rb
+++ app/views/automate/create_chef_node.rb
@@ -1,5 +1,5 @@
 # This script uses the Chef gem and the Chef Server API
-# To create a new node, assign it an IP Address and role
+# To create a new node, assign it an IP Address and role 
 
 require 'chef'
 require 'net/http'
