c94c7f04b7a1a2402060e868618406ee0927cdde
Michael Stack <michael.stack@gmail.com>
2015-02-20 19:31:29 -0600

    SAML credentials lookup
    - Rename some settings

--- app/controllers/saml_controller.rb
+++ app/controllers/saml_controller.rb
@@ -9,7 +9,7 @@ class SamlController < ApplicationController
     response = idp_response params
     response.settings = saml_settings request
     if response.is_valid?
-      user = Staff.find_by email: response.email
+      user = Staff.find_by email: response_email(response)
       return saml_failure if user.nil?
       sso_sign_in user
       redirect_to @settings[:redirect_url]
@@ -31,6 +31,14 @@ class SamlController < ApplicationController
     false
   end
 
+  def response_email(response)
+    [
+      response.name_id,
+      response.attributes['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
+      response.attributes['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn']
+    ].find { |v| v[/^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Z‌​a-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$/i] }
+  end
+
   def idp_response(params)
     OneLogin::RubySaml::Response.new(params[:SAMLResponse])
   end
@@ -41,8 +49,8 @@ class SamlController < ApplicationController
     settings.assertion_consumer_service_url = saml_consume_url host: request.host
     settings.issuer = "http://#{request.port == 80 ? request.host : request.host_with_port}"
     settings.idp_sso_target_url = @settings[:target_url]
+    settings.idp_cert = @settings[:certificate]
     settings.idp_cert_fingerprint = @settings[:fingerprint]
-    settings.name_identifier_format = 'urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified'
     settings.authn_context = 'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'
 
     settings
